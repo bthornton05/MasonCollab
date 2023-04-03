ea7b316778186e93a7320b1cb8a450d9f7a40de4
bthornton05 <bthornton05@ladueschools.net>
2023-04-03 04:03:21 +0000

    modified:   GoodCalculator/GoodCalculator.java

--- GoodCalculator/GoodCalculator.java
+++ GoodCalculator/GoodCalculator.java
@@ -11,7 +11,7 @@ public class GoodCalculator { //Made by Nickolas Holzer and Bradley Thornton wit
     public String getEq(){ //Returns equation
         return eq;
     }
-    private int getPreviousTerm(String eq) { //Finds previous integer term value preceding operator
+    private int findPreviousTerm(String eq) { //Finds previous integer term value preceding operator
         for(int i = eq.length() - 1; i>= 0; i--){
             if(!Character.isDigit(eq.charAt(i))){
                 return i + 1;
@@ -22,10 +22,10 @@ public class GoodCalculator { //Made by Nickolas Holzer and Bradley Thornton wit
         }
         return -1;
     }
-    private int getNextTerm(String eq) { //Finds integer term after operator
+    private int findNextTerm(String eq) { //Finds integer term after operator
         for(int i = 0; i< eq.length(); i++){
             if(!Character.isDigit(eq.charAt(i))){
-                return i - 1;
+                return i;
             }
             else if(i == eq.length() - 1){
                 return i + 1;
@@ -36,72 +36,83 @@ public class GoodCalculator { //Made by Nickolas Holzer and Bradley Thornton wit
 
     public int goodCalculate(String eq){ //Function that is called recursively to solve equation
         if(eq.indexOf('(') > -1){ //Finds index of closing and opening parentheses of inner most term to be solved
-            int pLeft = eq.indexOf('(');
+            int pLeft = -1;
             int pRight = -1;
             for(int i = eq.length() - 1; i>=0 ; i--){
-                if(eq.charAt(i) == ')'){
-                    pRight = i;
+                if(eq.charAt(i) == '('){
+                    pLeft = i;
+                    //System.out.println(pLeft);
                     break;
                 }
             }
-            String replaceTerm = eq.substring(pLeft, pRight + 1);
-            eq = eq.replace(replaceTerm, "" + goodCalculate(eq.substring(pLeft + 1, pRight))); //Calls method recursively with innermost term to solve equation
+            for(int j = pLeft; j < eq.length(); j++){
+                if(eq.charAt(j) == ')'){
+                    pRight = j;
+                    //System.out.println(pRight);
+                    break;
+                }
+            }
+            String replacedTerm = eq.substring(pLeft, pRight + 1);
+            System.out.println(replacedTerm);
+            eq = eq.replace(replacedTerm, "" + goodCalculate(eq.substring(pLeft + 1, pRight))); //Calls method recursively with innermost term to solve equation
             
         }
 
         while(eq.indexOf("^") > -1){ //Exponent operator
             int eOperator = eq.indexOf("^");
-            int previousTermIndex = getPreviousTerm(eq.substring(0,eOperator));
-            int nextTermIndex = eOperator + getNextTerm(eq.substring(eOperator + 1, eq.length()));
+            int previousTermIndex = findPreviousTerm(eq.substring(0,eOperator));
+            int nextTermIndex = eOperator + findNextTerm(eq.substring(eOperator + 1, eq.length()));
             int previousTerm = Integer.parseInt(eq.substring(previousTermIndex, eOperator));
             int nextTerm = Integer.parseInt(eq.substring(eOperator + 1, nextTermIndex+1));
             int term = (int)Math.pow(previousTerm, nextTerm);
             String miniTerm = eq.substring(previousTermIndex, nextTermIndex + 1);
             eq = eq.replace(miniTerm, "" + term);
+            System.out.println(eq);
         }
         while(eq.indexOf("*") > -1){ //Multiplication operator
             int eOperator = eq.indexOf("*");
-            int previousTermIndex = getPreviousTerm(eq.substring(0,eOperator));
-            int nextTermIndex = eOperator + getNextTerm(eq.substring(eOperator + 1, eq.length()));
+            int previousTermIndex = findPreviousTerm(eq.substring(0,eOperator));
+            int nextTermIndex = eOperator + findNextTerm(eq.substring(eOperator + 1, eq.length()));
             int previousTerm = Integer.parseInt(eq.substring(previousTermIndex, eOperator));
             int nextTerm = Integer.parseInt(eq.substring(eOperator + 1, nextTermIndex+1));
             int term = previousTerm * nextTerm;
             String miniTerm = eq.substring(previousTermIndex, nextTermIndex + 1);
             eq = eq.replace(miniTerm, "" + term);
+            System.out.println(eq);
         }
         while(eq.indexOf("/") > -1){ //Division operator
             int eOperator = eq.indexOf("/");
-            int previousTermIndex = getPreviousTerm(eq.substring(0,eOperator));
-            int nextTermIndex = eOperator + getNextTerm(eq.substring(eOperator + 1, eq.length()));
+            int previousTermIndex = findPreviousTerm(eq.substring(0,eOperator));
+            int nextTermIndex = eOperator + findNextTerm(eq.substring(eOperator + 1, eq.length()));
             int previousTerm = Integer.parseInt(eq.substring(previousTermIndex, eOperator));
             int nextTerm = Integer.parseInt(eq.substring(eOperator + 1, nextTermIndex+1));
             int term = previousTerm / nextTerm;
             String miniTerm = eq.substring(previousTermIndex, nextTermIndex + 1);
             eq = eq.replace(miniTerm, "" + term);
+            System.out.println(eq);
         }
         while(eq.indexOf("+") > -1){ //Addition operator
             int eOperator = eq.indexOf("+");
-            int previousTermIndex = getPreviousTerm(eq.substring(0,eOperator));
-            int nextTermIndex = eOperator + getNextTerm(eq.substring(eOperator + 1, eq.length()));
+            int previousTermIndex = findPreviousTerm(eq.substring(0,eOperator));
+            int nextTermIndex = eOperator + findNextTerm(eq.substring(eOperator + 1, eq.length()));
             int previousTerm = Integer.parseInt(eq.substring(previousTermIndex, eOperator));
             int nextTerm = Integer.parseInt(eq.substring(eOperator + 1, nextTermIndex+1));
             int term = previousTerm + nextTerm;
             String miniTerm = eq.substring(previousTermIndex, nextTermIndex + 1);
             eq = eq.replace(miniTerm, "" + term);
+            System.out.println(eq);
         }
         while(eq.indexOf("-") > -1){ //Subtraction operator
             int eOperator = eq.indexOf("-");
-            int previousTermIndex = getPreviousTerm(eq.substring(0,eOperator));
-            int nextTermIndex = eOperator + getNextTerm(eq.substring(eOperator + 1, eq.length()));
+            int previousTermIndex = findPreviousTerm(eq.substring(0,eOperator));
+            int nextTermIndex = eOperator + findNextTerm(eq.substring(eOperator + 1, eq.length()));
             int previousTerm = Integer.parseInt(eq.substring(previousTermIndex, eOperator));
             int nextTerm = Integer.parseInt(eq.substring(eOperator + 1, nextTermIndex+1));
             int term = previousTerm - nextTerm;
             String miniTerm = eq.substring(previousTermIndex, nextTermIndex + 1);
             eq = eq.replace(miniTerm, "" + term);
+            System.out.println(eq);
         }
-
         return Integer.parseInt(eq); //Returns string equation as an integer
+        }
     }
-   
-    
-}
